1430d8dbf93be6d4d1bacca25ed57b785ac658d0
Ahmad Kayyali <kayyali18@gmail.com>
2019-02-13 17:01:50 -0500

    Add dynamic route validation

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>A Single User with ID: {{ $route.params.id }}</h1>
+  <h1>A Single User with ID: {{ $route.params.id }} and the object of {{ info }}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: {}
+    }
+  },
+  validate(data) {
+    return parseInt(data.params.id) === 1
+  }
+}
+</script>
